a891eb5fae35713ac9256ec0a5952bf0380d5dba
Ventsislav Yordanov <venciity@abv.bg>
2014-10-20 23:25:25 +0300

    add Enemy.java and Explosion.java

---
+++ Crick-Survivor/src/Entity/Explosion.java
@@ -0,0 +1,82 @@
+package Entity;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class Explosion {
+	
+	private int x;
+	private int y;
+	private int xmap;
+	private int ymap;
+	
+	private int width;
+	private int height;
+	
+	private Animation animation;
+	private BufferedImage[] sprites;
+	
+	private boolean remove;
+	
+	public Explosion(int x, int y) {
+		
+		this.x = x;
+		this.y = y;
+		
+		width = 40;
+		height = 40;
+		
+		try {
+			
+			BufferedImage spritesheet = ImageIO.read(
+				getClass().getResourceAsStream(
+					"/Sprites/Enemies/explosion.png"
+				)
+			);
+			
+			sprites = new BufferedImage[7];
+			for(int i = 0; i < sprites.length; i++) {
+				sprites[i] = spritesheet.getSubimage(
+					i * width,
+					0,
+					width,
+					height
+				);
+			}
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		animation = new Animation();
+		animation.setFrames(sprites);
+		animation.setDelay(100);
+		
+	}
+	
+	public void update() {
+		animation.update();
+		if(animation.hasPlayedOnce()) {
+			remove = true;
+		}
+	}
+	
+	public boolean shouldRemove() { return remove; }
+	
+	public void setMapPosition(int x, int y) {
+		xmap = x;
+		ymap = y;
+	}
+	
+	public void draw(Graphics2D graphics) {
+		graphics.drawImage(
+			animation.getImage(),
+			x + xmap - width / 2,
+			y + ymap - height / 2,
+			null
+		);
+	}
+	
+}
